05173362e483669e1273f81cee5bf87f1b75e5be
Akuru <anjalismith0529@gmail.com>
2022-04-20 19:33:42 -0400

    ran successful test file

--- index.js
+++ index.js
@@ -5,67 +5,83 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
-      },
-      {
-        type: 'input',
-        message: 'What is your Github Username?',
-        name: 'username',
-      },
-      {
-        type: 'input',
-        message: 'What is your email address?',
-        name: 'email',
-      },
-      {
-        type: 'input',
-        message: 'Please write a description of your project.',
-        name: 'description',
-      },
-      {
-        type: 'input',
-        message: 'What command should I run to install dependencies?',
-        name: 'install',
-      },
-      {
-        type: 'input',
-        message: 'What command should I run for tests?',
-        name: 'test',
-      },
-      {
-        type: 'input',
-        message: 'What should your user need to know about using the repository?',
-        name: 'repo',
-      },
-      {
-        type: 'input',
-        message: 'Who contributed to this repo?',
-        name: 'contributors',
-      },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'What kind of license should your project have?',
-        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'ISC', 'None'],
-      },
-];
+const questions =
+    //inquirer 
+    // .prompt ([
+    [
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
+        },
+        {
+            type: 'input',
+            message: 'What is your Github Username?',
+            name: 'username',
+        },
+        {
+            type: 'input',
+            message: 'What is your email address?',
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: 'Please write a description of your project.',
+            name: 'description',
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'ISC', 'None'],
+        },
+        {
+            type: 'input',
+            message: 'What command should I run to install dependencies?',
+            name: 'install',
+        },
+        {
+            type: 'input',
+            message: 'What command should I run for tests?',
+            name: 'test',
+        },
+        {
+            type: 'input',
+            message: 'What should your user need to know about using the repository?',
+            name: 'repo',
+        },
+        {
+            type: 'input',
+            message: 'Who contributed to this repo?',
+            name: 'contributors',
+        },
+    ]
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-/*          fs.writeFile('data.html', info, (err) => {
-            err ? console.log(err) : console.log('success')
-        })
+function writeToFile(fileName, data) {
+    inquirer
+        .prompt(questions)
 
-    }).catch((err) => {
-        if (err) throw err;
-    });
+        .then((data) => {
+
+            // use markdown syntax? instead of html :(
+            let info = 
+
+            fs.writeFile('README.md', info, (err) => {
+                err ? console.log(err) : console.log('Generating README...')
+            })
+        }).catch((err) => {
+            if (err) throw err;
+        });
+
+};
+
+writeToFile();
+
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() { }
 
 // Function call to initialize app
-init();
+//init();
