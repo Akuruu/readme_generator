f7e31ee7ea42e9cb0d507551699ac554025365e2
Akuru <anjalismith0529@gmail.com>
2022-04-21 17:42:23 -0400

    cleaned up License information

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  // link for building the badge
-  // `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   if (license !== '') {
     return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
   }
   return "";
 }
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license !== '') {
-    return `\n * [License](#) \n`
-  }
-  return "";
- 
- }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -25,7 +13,7 @@ function renderLicenseSection(license) {
   if (license !== '') {
     return `## License
     
-    this project is licensed under ${data.license}.`
+    this project is licensed under ${renderLicenseBadge()}.`
   }
   return "";
   
@@ -35,15 +23,15 @@ function renderLicenseSection(license) {
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
        return `# ${data.title}
-       \n ## Table of Contents \n
-       \n-----------------------------------------\n
-     \n - [Description](#description) \n
-     \n - [Showcase](#link) \n
-     \n - [Installation Instructions](#install)\n
-     \n - [Usage Information](#usage) \n
-     \n - [Licenses](#license) \n
-     \n - [Contributors](#contributors) \n
-     \n - [Questions?](#username) \n
+      \n ## Table of Contents \n
+      \n-----------------------------------------\n
+      \n - [Description](#description) \n
+      \n - [Showcase](#link) \n
+      \n - [Installation Instructions](#install)\n
+      \n - [Usage Information](#usage) \n
+      \n - [Licenses](#license) \n
+      \n - [Contributors](#contributors) \n
+      \n - [Questions?](#username) \n
 
       \n ## Description \n
       \n-----------------------------------------\n
@@ -53,26 +41,26 @@ function generateMarkdown(data) {
       \n-----------------------------------------\n 
       \n (${data.link})\n
 
-    \n ## Installation Instructions\n
-    \n-----------------------------------------\n 
-    \n ${data.install}\n
+      \n ## Installation Instructions\n
+      \n-----------------------------------------\n 
+      \n ${data.install}\n
 
       \n ## Usage Information \n
       \n-----------------------------------------\n 
       \n  ${data.usage}\n
 
-     \n  ## Licenses \n
-     \n-----------------------------------------\n 
-     \n -  ${renderLicenseBadge(data.license)}\n
+      \n ## License \n
+      \n-----------------------------------------\n 
+      \n  ${renderLicenseBadge(data.license)}\n
 
-       \n ## Contributors\n
-       \n-----------------------------------------\n 
-       \n - [${data.contributors}]\n
+      \n ## Contributors\n
+      \n-----------------------------------------\n 
+      \n  ${data.contributors}\n
 
-       \n ## Questions?\n
-       \n-----------------------------------------\n 
+      \n ## Questions?\n
+      \n-----------------------------------------\n 
       \n  My Github: [${data.username}](${data.gitlink})\n
-       \n Contact Me: ${data.email} \n`   
+      \n Contact Me: ${data.email} \n`   
             
 };
 
