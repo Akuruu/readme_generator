a655b501437e1f677110a6bb4baaadb7d7f47627
Akuru <anjalismith0529@gmail.com>
2022-04-21 17:14:47 -0400

    linked working license badge banner

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,7 +3,7 @@
 function renderLicenseBadge(license) {
   // link for building the badge
   // `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
-  if (license !== None) {
+  if (license !== '') {
     return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
   }
   return "";
@@ -12,67 +12,69 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license !== None) {
+  if (license !== '') {
     return `\n * [License](#) \n`
   }
   return "";
+ 
  }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) { 
-  if (license !== None) {
+  if (license !== '') {
     return `## License
     
-    this project is licensed under ${license}.`
+    this project is licensed under ${data.license}.`
   }
   return "";
   
 }
 
+
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-            \n ## Table of Contents \n
-            \n-----------------------------------------\n
-          \n - [Description](#description) \n
-          \n - [Showcase](#link) \n
-          \n - [Installation Instructions](#install\n
-          \n - [Usage Information](#usage) \n
-          \n - [Licenses](#license) \n
-          \n - [Contributors](#contributors) \n
-          \n - [Questions?](#username) \n
+       return `# ${data.title}
+       \n ## Table of Contents \n
+       \n-----------------------------------------\n
+     \n - [Description](#description) \n
+     \n - [Showcase](#link) \n
+     \n - [Installation Instructions](#install)\n
+     \n - [Usage Information](#usage) \n
+     \n - [Licenses](#license) \n
+     \n - [Contributors](#contributors) \n
+     \n - [Questions?](#username) \n
 
-           \n ## Description \n
-           \n-----------------------------------------\n
-           \n ${data.description}\n
+      \n ## Description \n
+      \n-----------------------------------------\n
+      \n ${data.description}\n
 
-           \n ## Showcase\n
-           \n-----------------------------------------\n 
-           \n ![alt text](${data.link})\n
+      \n ## Showcase\n
+      \n-----------------------------------------\n 
+      \n (${data.link})\n
 
-         \n ## Installation Instructions\n
-         \n-----------------------------------------\n 
-         \n ${data.install}\n
+    \n ## Installation Instructions\n
+    \n-----------------------------------------\n 
+    \n ${data.install}\n
 
-           \n ## Usage Information \n
-           \n-----------------------------------------\n 
-           \n  ${data.usage}\n
+      \n ## Usage Information \n
+      \n-----------------------------------------\n 
+      \n  ${data.usage}\n
 
-          \n  ## Licenses \n
-          \n-----------------------------------------\n 
-          \n ${data.license}\n
+     \n  ## Licenses \n
+     \n-----------------------------------------\n 
+     \n -  ${renderLicenseBadge(data.license)}\n
 
-            \n ## Contributors\n
-            \n-----------------------------------------\n 
-            \n - [${data.contributors}]()\n
+       \n ## Contributors\n
+       \n-----------------------------------------\n 
+       \n - [${data.contributors}]\n
 
-            \n ## Questions?\n
-            \n-----------------------------------------\n 
-           \n  My Github: [${data.username}](${data.gitlink})\n
-            \n Contact Me: ${data.email} \n`
+       \n ## Questions?\n
+       \n-----------------------------------------\n 
+      \n  My Github: [${data.username}](${data.gitlink})\n
+       \n Contact Me: ${data.email} \n`   
+            
 };
-
 
 module.exports = generateMarkdown;
 
