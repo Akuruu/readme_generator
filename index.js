ddb744a635d4026eb2c5ed44006b902fa0016693
Akuru <anjalismith0529@gmail.com>
2022-04-20 20:18:14 -0400

    added more markdown syntax that links to data

--- index.js
+++ index.js
@@ -6,53 +6,58 @@ const path = require('path');
 
 // TODO: Create an array of questions for user input
 const questions = [
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: 'What is your Github Username?',
-            name: 'username',
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email',
-        },
-        {
-            type: 'input',
-            message: 'Please write a description of your project.',
-            name: 'description',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'What kind of license should your project have?',
-            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'ISC', 'None'],
-        },
-        {
-            type: 'input',
-            message: 'What command should I run to install dependencies?',
-            name: 'install',
-        },
-        {
-            type: 'input',
-            message: 'What command should I run for tests?',
-            name: 'test',
-        },
-        {
-            type: 'input',
-            message: 'What should your user need to know about using the repository?',
-            name: 'repo',
-        },
-        {
-            type: 'input',
-            message: 'Who contributed to this repo?',
-            name: 'contributors',
-        },
-    ];
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github Username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Please write a description of your project.',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please insert any img or video urls to showcase your project.',
+        name: 'link',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'ISC', 'None'],
+    },
+    {
+        type: 'input',
+        message: 'What command should I run to install dependencies?',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'What command should I run for tests?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'What should your user need to know about using the repository?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Who contributed to this repo?',
+        name: 'contributors',
+    },
+];
 
 
 // TODO: Create a function to write README file
@@ -60,14 +65,43 @@ function writeToFile(fileName, data) {
     inquirer
         .prompt(questions)
 
-        .then((data) => { 
+        .then((data) => {
 
             // use markdown syntax? instead of html :(
-            let info = `\n # ${data.title} \n
+            let info = `
+            \n # ${data.title} \n
             \n-----------------------------------------\n 
+            \n ## Table of Contents \n
+            \n-----------------------------------------\n 
+          \n - Description \n
+          \n - Showcase \n
+          \n - Installation Instructions\n
+          \n - Usage Information \n
+          \n - Licenses \n
+          \n - Contributors \n
+          \n - Questions? \n
+
            \n ## Description \n
-           \n-----------------------------------------\n 
-           > ${data.description}
+            ${data.description}
+
+           \n ## Showcase\n
+            ![alt text](${data.link})
+
+         \n ## Installation Instructions\n
+            ${data.install}
+
+           \n ## Usage Information \n
+             ${data.usage}
+
+          \n  ## Licenses \n
+            ${data.license}
+
+            \n ## Contributors\n
+            - [${data.contributors}]()
+
+            \n ## Questions?\n
+           \n  My Github: [${data.username}](${data.username})\n
+            \n Contact Me: [${data.username}](${data.username}}) \n
             `
 
             fs.writeFile('README.md', info, (err) => {
